74942070a4f0ec4825a2eca6f40cf6c98e0cf433
frootwale <you@example.com>
2014-06-05 12:05:27 +0530

    First commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruitswale::Application.config.secret_token = '57b2b7e74a436b7730e6cb9425ecf5d1b7ac38b00e84bd3c905a30c5817ffea2d71db2c2a9887f3b77fba9425846e3b6fdfddb9690392ecfd496b8f16035a337'
